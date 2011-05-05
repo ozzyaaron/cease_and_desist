1fd8a8ac35e03f680a86b10684711c07c8362ecf
ozzyaaron <aaron@thefrontiergroup.com.au>
2011-05-05 21:29:17 +0800

    Initial commit of rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CeaseAndDesist::Application.config.secret_token = '309b432b18d3c9558a1a98cc93be5e2724f145d5db93f7d411a1dd2d0c33567c4ab0e457179c3f5d94468643cc3b5dac1c9f3ad4d9632b15ac3901d44000de00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CeaseAndDesist::Application.config.session_store :cookie_store, :key => '_cease_and_desist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CeaseAndDesist::Application.config.session_store :active_record_store
